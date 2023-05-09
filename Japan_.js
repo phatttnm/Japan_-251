b437cbfda268300e3bd6568263a041099b2243e2
phatttnm <124548061+phatttnm@users.noreply.github.com>
2023-05-09 10:57:20 +0700

    Add files via upload

---
+++ Japan_.js
@@ -0,0 +1,80 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Osaka castle",
+          "ประเภท": "สถานที่ท่องเที่ยว",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            135.52584200000342,
+            34.68732452376544
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Universal studio japan",
+          "ประเภท": "สวนสนุก",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            135.4318148449496,
+            34.66648272165865
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Dotonbori",
+          "ประเภท": "ตลาด",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            135.49883712514634,
+            34.668655702755274
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Sumiyoshi Taisha",
+          "ประเภท": "ศาลเจ้า",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            135.4929474365219,
+            34.61279163263538
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Hozenji",
+          "ประเภท": "ศาลเจ้า",
+          "ระดับ": 7
+        },
+        "geometry": {
+          "coordinates": [
+            135.5026143125657,
+            34.66785577622707
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
